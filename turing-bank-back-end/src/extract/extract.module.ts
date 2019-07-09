26c759a3d5cb702b87ee0c75aaa3e4e48ae428b9
Avanade <Avanade@WIN-Q7AHOIVCJY.corp.avanade.org>
2019-07-08 22:23:10 -0300

    remove folder/module extract

--- turing-bank-back-end/src/extract/extract.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExtractController } from './extract.controller';
-
-@Module({
-  controllers: [ExtractController]
-})
-export class ExtractModule {}
