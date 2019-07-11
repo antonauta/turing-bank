ec217dae7dbdabc725d0b11df0e6bd891382fd70
acamilass <acamilass@gmail.com>
2019-07-11 19:26:36 -0300

    adicionando home

---
+++ turing-bank-front-end/src/app/pages/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterModule } from 'src/app/components/footer/footer.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FooterModule
+  ]
+})
+export class HomeModule { }
