e195636217155f480deb16b3e254fc3b9bc7e7fa
Avanade <antonauta@pm.me>
2019-07-11 15:21:29 -0300

    Add init config users.service.spec.ts

--- turing-bank-back-end/src/users/users.service.spec.ts
+++ turing-bank-back-end/src/users/users.service.spec.ts
@@ -1,18 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { mockgooseProvider } from '../../test/mockoose.provider';
+import { UserSchema } from './schemas/user.schema';
+import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose'
+import { Model } from 'mongoose';
+import { async } from 'rxjs/internal/scheduler/async';
+import { UsersController } from './users.controller';
 
-describe('UsersService', () => {
-  let service: UsersService;
+describe('UserService', async () => {
+  let userService: UsersService;
+  let userModel: Model<User>
+  const token = getModelToken(UserSchema);
+  const userProvider = {
+    provide: token,
+    useFactory: async connection => connection.model('User', UserSchema),
+    inject: ['DbConnectionToken'],
+  } as any;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+  beforeAll(async () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    const module = await Test.createTestingModule({
+      controllers: [UsersController]
     }).compile();
-
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
+    userModel = module.get(token);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+
 });
