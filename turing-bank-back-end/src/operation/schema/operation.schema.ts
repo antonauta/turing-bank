5d129b72a92427173574d0f0be3d1f7ad35ce436
guilherme <guilhermetyamamoto@gmail.com>
2019-07-11 11:21:01 -0300

    adicionado tempo de expiracao para dados de operacoes

--- turing-bank-back-end/src/operation/schema/operation.schema.ts
+++ turing-bank-back-end/src/operation/schema/operation.schema.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 export const operationSchema = new mongoose.Schema({
     type: { type: Number },
     value: { type: Number, default: 0},
+    expire_at: {type: Date, default: Date.now, expires: 6.3113904 * 10 ^10} , //  dados de op duram 2 anos
     date: { type: Date, default: new Date() },
     origin: { type: mongoose.Schema.ObjectId, ref: 'User' },
     destin: { type: mongoose.Schema.ObjectId, ref: 'User' },
