b401111ebc39d8fc6b493898e1a1b7765fc1d9a6
Avanade <Avanade@WIN-19WSFV125B.corp.avanade.org>
2019-07-12 15:56:57 -0300

    correcao compose

--- turing-bank-front-end/src/app/pages/login/login.module.ts
+++ turing-bank-front-end/src/app/pages/login/login.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatStepperModule, MatInputModule } from '@angular/material';
-=======
-
-
->>>>>>> 4543744ce4ad5ba03f63d166dcb5c9de41aa6967
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-<<<<<<< HEAD
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
@@ -18,8 +12,6 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatListModule,
     MatStepperModule,
     MatInputModule
-=======
->>>>>>> 4543744ce4ad5ba03f63d166dcb5c9de41aa6967
   ]
 })
 export class LoginModule { }
