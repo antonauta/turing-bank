8f9a26e4cfb4e4555d980e2276dad4097bf7b0bc
Caio Santos <caio.santos687@gmail.com>
2019-07-12 16:39:39 -0300

    init auth service

---
+++ turing-bank-front-end/src/app/core/interfaces/services/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { URL } from '../api/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  constructor() { }
+
+
+}
