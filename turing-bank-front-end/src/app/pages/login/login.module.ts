5ed55cc3e254dbb26c87e203922f99eab2bca954
Avanade <antonauta@pm.me>
2019-07-12 14:38:51 -0300

    change filterDate

--- turing-bank-front-end/src/app/pages/login/login.module.ts
+++ turing-bank-front-end/src/app/pages/login/login.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatStepperModule, MatInputModule } from '@angular/material';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatStepperModule,
+    MatInputModule
   ]
 })
 export class LoginModule { }
