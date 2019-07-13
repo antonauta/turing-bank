aa64f93501aa20ea97c91eaa5b4a07d7b12fe492
Avanade <antonauta@pm.me>
2019-07-13 02:31:18 -0300

    Add more testing

--- turing-bank-back-end/test/app.e2e-spec.ts
+++ turing-bank-back-end/test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 import * as request from 'supertest';
 import { RegisterDTO, LoginDTO } from '../src/infra/auth/dto/auth.dto'
 import { app, database } from './constants'
+import { CreateOperationDto } from '../src/operation/dto/create.operation.dto';
 
 beforeAll(async () => {
   const t = await mongoose.connect(database);
@@ -22,10 +23,21 @@ describe('AUTH', () => {
     cpf: "33366699900"
   };
 
+  const operationDeposit: CreateOperationDto = {
+    type: 0,
+    value: 100
+  };
+
   const loginAccount: LoginDTO = {
     cpf: "33366699900",
     password: "123456"
   }
+
+  const operationTranfer: CreateOperationDto = {
+    type: 1,
+    value: 100,
+    destination: "5d295a9c3648bc050ca960d8"
+  };
 
   let token: string;
   it('Should register user', async () => {
@@ -57,15 +69,34 @@ describe('AUTH', () => {
 
   it('should login user', () => {
     return request(app)
-    .post('/auth/login')
-    .set('Accept', 'application/json')
-    .send(loginAccount)
-    .expect(({ body }) => {
-      token = body.token;
+      .post('/auth/login')
+      .set('Accept', 'application/json')
+      .send(loginAccount)
+      .expect(({ body }) => {
+        token = body.token;
 
-      expect(body.token).toBeDefined();
-      expect(body.user.name).toEqual('Antônio Flávio Sousa Silva Nascimento Júnior');
-      expect(body.user.password).toBeDefined();
-    })
+        expect(body.token).toBeDefined();
+        expect(body.user.name).toEqual('Antônio Flávio Sousa Silva Nascimento Júnior');
+        expect(body.user.password).toBeDefined();
+      })
   })
+
+  // describe('OPERATION', () => {
+  //   it('Should register operation', async () => {
+  //     let token = ""
+  //     await request(app).post('/auth/register').set('Accept', 'application/json').send(registerAccount)
+  //       .expect(({ body }) => {
+  //         token = body.token;
+  //         return request(app)
+  //           .post('/operation')
+  //           .set('Authorization', `bearer ${token}`)
+  //           .set('Accept', 'application/json')
+  //           .send(operationDeposit)
+  //           .expect(({ body }) => {
+  //             console.log(body);
+  //             expect(body.value).toEqual(100)
+  //           })
+  //       })
+  //   })
+  // })
 });
