d672500bbd0f9faf18a4158c95267f72f4b3f1fb
VagnerSilva <vagner23silva@gmail.com>
2019-07-14 06:28:17 -0300

    transferencia com form reativo

--- turing-bank-front-end/src/app/pages/transferencia/transferencia.component.ts
+++ turing-bank-front-end/src/app/pages/transferencia/transferencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { UserValidatorInterface } from 'src/app/core/interfaces/validations/user.validator.interface';
+import { NotificationServiceInterface } from 'src/app/core/interfaces/services/notification/notification.service.interface';
 
 @Component({
   selector: 'app-transferencia',
@@ -11,9 +14,40 @@ export class TransferenciaComponent implements OnInit {
 
   toggleOptions: Array<String> = ["Conta Corrente", "Conta Poupança"];
 
-  constructor() { }
+  agencia = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(80),
+  ]);
+
+  conta = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(80),
+  ]);
+
+  valor = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(80),
+  ]);
+
+  transferenciaForm: FormGroup;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private userValidatorInterface: UserValidatorInterface,
+    private notificationServiceInterface: NotificationServiceInterface,
+  ) { }
 
   ngOnInit() {
+
+    this.transferenciaForm = this.fb.group({
+      agencia: this.agencia,
+      conta: this.conta,
+      valor: this.valor
+    });
+  }
+
+  submit() {
   }
 
 }
